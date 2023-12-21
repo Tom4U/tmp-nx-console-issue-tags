c98598a449668141cff4e9e6c709abd04ddb7ddf
Tom Ohms <thomas@ohms.pro>
2023-12-21 10:53:04 +0100

    doc(nx): add dep checks sample and log

--- libs/src/lib/my-lib1.ts
+++ libs/src/lib/my-lib1.ts
@@ -1,3 +1,9 @@
+import { validate } from 'compare-versions';
+
 export function myLib1(): string {
   return 'my-lib1';
+}
+
+export function doSomethingWithCompareVersion() {
+  return validate('1.0.0');
 }
